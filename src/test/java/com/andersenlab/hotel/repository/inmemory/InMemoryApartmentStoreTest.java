6deff33baa296249fc4ee8750dc89e6eac360743
AnnYaniv <yaniv.anya@gmail.com>
2023-10-18 15:43:39 +0200

    Test ApartmentRepository and ApartmentService Fix adjust method

--- src/test/java/com/andersenlab/hotel/repository/inmemory/InMemoryApartmentStoreTest.java
+++ src/test/java/com/andersenlab/hotel/repository/inmemory/InMemoryApartmentStoreTest.java
@@ -1,5 +1,8 @@
 package com.andersenlab.hotel.repository.inmemory;
 
+import com.andersenlab.hotel.model.Apartment;
+import com.andersenlab.hotel.model.ApartmentStatus;
+import com.andersenlab.hotel.repository.ApartmentSort;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,166 +10,157 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class InMemoryApartmentStoreTest {
-//    InMemoryApartmentStore target;
-//    ApartmentStore.ApartmentEntity apartment1;
-//    ApartmentStore.ApartmentEntity apartment2;
-//    ApartmentStore.ApartmentEntity apartment3;
-//    ApartmentStore.ApartmentEntity apartment4;
-//    ApartmentStore.ApartmentEntity apartment5;
-//
-//    @BeforeEach
-//    void setUp() {
-//        target = new InMemoryApartmentStore();
-//
-//        apartment1 = new ApartmentStore.
-//                ApartmentEntity(UUID.randomUUID(), BigDecimal.ONE, BigInteger.ONE,
-//                true, ApartmentStore.ApartmentStatus.AVAILABLE);
-//
-//        apartment2 = new ApartmentStore.
-//                ApartmentEntity(UUID.randomUUID(), BigDecimal.TWO, BigInteger.TWO,
-//                true, ApartmentStore.ApartmentStatus.CLOSED);
-//
-//        apartment3 = new ApartmentStore.
-//                ApartmentEntity(UUID.fromString("9d76f43e-4ce2-40ba-90e1-37a6bc425ee1"),
-//                BigDecimal.TEN, BigInteger.TEN, true,
-//                ApartmentStore.ApartmentStatus.AVAILABLE);
-//
-//        apartment4 = new ApartmentStore.
-//                ApartmentEntity(UUID.fromString("9d76f43e-4ce2-40ba-90e1-37a6bc425ee2"),
-//                BigDecimal.ONE, BigInteger.TWO, true, ApartmentStore.ApartmentStatus.AVAILABLE);
-//
-//        apartment5 = new ApartmentStore.
-//                ApartmentEntity(UUID.fromString("9d76f43e-4ce2-40ba-90e1-37a6bc425ee3"),
-//                BigDecimal.TWO, BigInteger.ONE, false, ApartmentStore.ApartmentStatus.AVAILABLE);
-//    }
-//
-//    @Test
-//    void save_SingleEntity_ShouldSaveEntityInStore() {
-//        target.save(apartment1);
-//
-//        int actualSize = target.findAllSorted(ApartmentStore.Sort.ID).size();
-//
-//        assertEquals(1, actualSize);
-//        assertTrue(target.has(apartment1.id()));
-//    }
-//
-//    @Test
-//    void save_MultipleEntity_ShouldSaveEntitiesInStore() {
-//        target.save(apartment2);
-//        target.save(apartment3);
-//
-//        int actualSize = target.findAllSorted(ApartmentStore.Sort.ID).size();
-//
-//        assertEquals(2, actualSize);
-//        assertTrue(target.has(apartment3.id()));
-//        assertTrue(target.has(apartment2.id()));
-//    }
-//
-//    @Test
-//    void delete_ExistingEntity_ShouldDeleteEntityFromStore() {
-//        target.save(apartment1);
-//
-//        target.delete(apartment1.id());
-//
-//        assertFalse(target.has(apartment1.id()));
-//    }
-//
-//    @Test
-//    void delete_NonExistingEntity_ShouldDeleteEntityFromStore() {
-//        target.delete(apartment1.id());
-//
-//        assertFalse(target.has(apartment1.id()));
-//    }
-//
-//    @Test
-//    void getById_ExistingEntity_ShouldReturnEntityFromStore() {
-//        target.save(apartment1);
-//
-//        ApartmentStore.ApartmentEntity actual = target.getById(apartment1.id());
-//
-//        assertEquals(apartment1, actual);
-//    }
-//
-//    @Test
-//    void getById_ExistingEntity_ShouldReturnNullEntity() {
-//        ApartmentStore.ApartmentEntity actual = target.getById(apartment1.id());
-//
-//        Assertions.assertNotEquals(apartment1, actual);
-//        Assertions.assertNull(actual);
-//    }
-//
-//    @Test
-//    void findAllSorted_EntityId_ShouldSortEntitiesFromStoreById() {
-//        target.save(apartment3);
-//        target.save(apartment4);
-//        target.save(apartment5);
-//
-//        List<ApartmentStore.ApartmentEntity> sorted = (List<ApartmentStore.ApartmentEntity>)
-//                target.findAllSorted(ApartmentStore.Sort.ID);
-//
-//        assertEquals(apartment3, sorted.get(0));
-//        assertEquals(apartment4, sorted.get(1));
-//        assertEquals(apartment5, sorted.get(2));
-//    }
-//
-//    @Test
-//    void findAllSorted_EntityPrice_ShouldSortEntitiesFromStoreByPrice() {
-//        target.save(apartment3);
-//        target.save(apartment4);
-//        target.save(apartment5);
-//
-//        List<ApartmentStore.ApartmentEntity> sorted = (List<ApartmentStore.ApartmentEntity>)
-//                target.findAllSorted(ApartmentStore.Sort.PRICE);
-//
-//        assertEquals(apartment4, sorted.get(0));
-//        assertEquals(apartment5, sorted.get(1));
-//        assertEquals(apartment3, sorted.get(2));
-//    }
-//
-//    @Test
-//    void findAllSorted_EntityCapacity_ShouldSortEntitiesFromStoreByCapacity() {
-//        target.save(apartment3);
-//        target.save(apartment4);
-//        target.save(apartment5);
-//
-//        List<ApartmentStore.ApartmentEntity> sorted = (List<ApartmentStore.ApartmentEntity>)
-//                target.findAllSorted(ApartmentStore.Sort.CAPACITY);
-//
-//        assertEquals(apartment5, sorted.get(0));
-//        assertEquals(apartment4, sorted.get(1));
-//        assertEquals(apartment3, sorted.get(2));
-//    }
-//
-//    @Test
-//    void findAllSorted_EntityAvailability_ShouldSortEntitiesFromStoreByAvailability() {
-//        target.save(apartment5);
-//        target.save(apartment3);
-//        target.save(apartment4);
-//
-//        List<ApartmentStore.ApartmentEntity> sorted = (List<ApartmentStore.ApartmentEntity>)
-//                target.findAllSorted(ApartmentStore.Sort.AVAILABILITY);
-//
-//        assertFalse(sorted.get(0).availability());
-//        assertTrue(sorted.get(1).availability());
-//        assertTrue(sorted.get(2).availability());
-//    }
-//
-//    @Test
-//    void has_ExistingEntity_ShouldReturnTrue() {
-//        target.save(apartment1);
-//
-//        assertTrue(target.has(apartment1.id()));
-//    }
-//
-//    @Test
-//    void has_NonExistingEntity_ShouldReturnFalse() {
-//        assertFalse(target.has(apartment1.id()));
-//    }
+
+    InMemoryApartmentRepository target;
+    Apartment apartment1, apartment2, apartment3, apartment4, apartment5;
+
+    @BeforeEach
+    void setUp() {
+        target = new InMemoryApartmentRepository();
+
+        apartment1 = new Apartment(UUID.randomUUID(), BigDecimal.ONE, BigInteger.ONE,
+                true, ApartmentStatus.AVAILABLE);
+
+        apartment2 = new Apartment(UUID.randomUUID(), BigDecimal.TWO, BigInteger.TWO,
+                true, ApartmentStatus.RESERVED);
+
+        apartment3 = new Apartment(UUID.fromString("9d76f43e-4ce2-40ba-90e1-37a6bc425ee1"),
+                BigDecimal.TEN, BigInteger.TEN, true, ApartmentStatus.AVAILABLE);
+
+        apartment4 = new Apartment(UUID.fromString("9d76f43e-4ce2-40ba-90e1-37a6bc425ee2"),
+                BigDecimal.ONE, BigInteger.TWO, true, ApartmentStatus.AVAILABLE);
+
+        apartment5 = new Apartment(UUID.fromString("9d76f43e-4ce2-40ba-90e1-37a6bc425ee3"),
+                BigDecimal.TWO, BigInteger.ONE, false, ApartmentStatus.AVAILABLE);
+    }
+
+    @Test
+    void save_SingleEntity_ShouldSaveEntityInStore() {
+        target.save(apartment1);
+
+        int actualSize = target.findAllSorted(ApartmentSort.ID).size();
+
+        assertThat(actualSize).isEqualTo(1);
+        assertTrue(target.has(apartment1.getId()));
+    }
+
+    @Test
+    void save_MultipleEntity_ShouldSaveEntitiesInStore() {
+        target.save(apartment2);
+        target.save(apartment3);
+
+        int actualSize = target.findAllSorted(ApartmentSort.ID).size();
+
+        assertThat(actualSize).isEqualTo(2);
+        assertTrue(target.has(apartment3.getId()));
+        assertTrue(target.has(apartment2.getId()));
+    }
+
+    @Test
+    void delete_ExistingEntity_ShouldDeleteEntityFromStore() {
+        target.save(apartment1);
+
+        target.delete(apartment1.getId());
+
+        assertFalse(target.has(apartment1.getId()));
+    }
+
+    @Test
+    void delete_NonExistingEntity_ShouldDeleteEntityFromStore() {
+        target.delete(apartment1.getId());
+
+        assertFalse(target.has(apartment1.getId()));
+    }
+
+    @Test
+    void getById_ExistingEntity_ShouldReturnEntityFromStore() {
+        target.save(apartment1);
+        Optional<Apartment> actual = target.getById(apartment1.getId());
+
+        assertTrue(actual.isPresent());
+        assertThat(actual).contains(apartment1);
+    }
+
+    @Test
+    void getById_ExistingEntity_ShouldReturnEmptyOptional() {
+        Optional<Apartment> actual = target.getById(apartment1.getId());
+
+        Assertions.assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    void findAllSorted_EntityId_ShouldSortEntitiesFromStoreById() {
+        target.save(apartment3);
+        target.save(apartment4);
+        target.save(apartment5);
+
+        List<Apartment> sorted = (List<Apartment>)
+                target.findAllSorted(ApartmentSort.ID);
+
+        assertThat(sorted.get(0)).isEqualTo(apartment3);
+        assertThat(sorted.get(1)).isEqualTo(apartment4);
+        assertThat(sorted.get(2)).isEqualTo(apartment5);
+    }
+
+    @Test
+    void findAllSorted_EntityPrice_ShouldSortEntitiesFromStoreByPrice() {
+        target.save(apartment3);
+        target.save(apartment4);
+        target.save(apartment5);
+
+        List<Apartment> sorted = (List<Apartment>)
+                target.findAllSorted(ApartmentSort.PRICE);
+
+        assertThat(sorted.get(0)).isEqualTo(apartment4);
+        assertThat(sorted.get(1)).isEqualTo(apartment5);
+        assertThat(sorted.get(2)).isEqualTo(apartment3);
+    }
+
+    @Test
+    void findAllSorted_EntityCapacity_ShouldSortEntitiesFromStoreByCapacity() {
+        target.save(apartment3);
+        target.save(apartment4);
+        target.save(apartment5);
+
+        List<Apartment> sorted = (List<Apartment>)
+                target.findAllSorted(ApartmentSort.CAPACITY);
+
+        assertThat(sorted.get(0)).isEqualTo(apartment5);
+        assertThat(sorted.get(1)).isEqualTo(apartment4);
+        assertThat(sorted.get(2)).isEqualTo(apartment3);
+    }
+
+    @Test
+    void findAllSorted_EntityAvailability_ShouldSortEntitiesFromStoreByAvailability() {
+        target.save(apartment5);
+        target.save(apartment3);
+        target.save(apartment4);
+
+        List<Apartment> sorted = (List<Apartment>)
+                target.findAllSorted(ApartmentSort.AVAILABILITY);
+
+        assertFalse(sorted.get(0).isAvailability());
+        assertTrue(sorted.get(1).isAvailability());
+        assertTrue(sorted.get(2).isAvailability());
+    }
+
+    @Test
+    void has_ExistingEntity_ShouldReturnTrue() {
+        target.save(apartment1);
+
+        assertTrue(target.has(apartment1.getId()));
+    }
+
+    @Test
+    void has_NonExistingEntity_ShouldReturnFalse() {
+        assertFalse(target.has(apartment1.getId()));
+    }
 }
--- src/test/java/com/andersenlab/hotel/service/ApartmentServiceUnitTest.java
+++ src/test/java/com/andersenlab/hotel/service/ApartmentServiceUnitTest.java
@@ -2,16 +2,22 @@ package com.andersenlab.hotel.service;
 
 
 import com.andersenlab.hotel.model.Apartment;
+import com.andersenlab.hotel.model.ApartmentStatus;
+import com.andersenlab.hotel.repository.ApartmentSort;
 import com.andersenlab.hotel.repository.inmemory.InMemoryApartmentRepository;
 import com.andersenlab.hotel.service.impl.ApartmentService;
 import com.andersenlab.hotel.usecase.exception.ApartmentNotfoundException;
-import org.junit.jupiter.api.Assertions;
+import com.andersenlab.hotel.usecase.exception.ApartmentWithSameIdExists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -22,27 +28,152 @@ class ApartmentServiceUnitTest {
     ApartmentService target;
     InMemoryApartmentRepository repo;
 
-    Apartment apartment;
+    Apartment apartmentAvailable, apartmentUnavailable;
+    UUID apartmentAvailableId, apartmentUnavailableId;
+
     @BeforeEach
     void setUp() {
         repo = mock(InMemoryApartmentRepository.class);
         target = new ApartmentService(repo);
+        apartmentUnavailableId = UUID.randomUUID();
+        apartmentAvailableId = UUID.randomUUID();
+        apartmentAvailable = new Apartment(apartmentAvailableId, BigDecimal.ONE, BigInteger.ONE,
+                true, ApartmentStatus.AVAILABLE);
+
+        apartmentUnavailable = new Apartment(apartmentUnavailableId, BigDecimal.TWO, BigInteger.TWO,
+                false, ApartmentStatus.RESERVED);
     }
 
     @Test
     void delete_NotExistingId_ShouldThrowsApartmentNotFoundException() {
         final UUID id = UUID.randomUUID();
+        when(repo.has(any(UUID.class))).thenReturn(false);
 
         assertThrows(ApartmentNotfoundException.class, () -> target.delete(id));
     }
 
     @Test
-    void delete_ExistingId_ShouldCallRepositoryMethod(){
+    void delete_ExistingId_ShouldCallRepositoryMethod() {
         final UUID id = UUID.randomUUID();
         when(repo.has(any(UUID.class))).thenReturn(true);
 
-        repo.delete(id);
+        target.delete(id);
 
         verify(repo).delete(any());
+    }
+
+    @Test
+    void has_AnyUuid_ShouldCallRepositoryMethod() {
+        final UUID id = UUID.randomUUID();
+
+        target.has(id);
+
+        verify(repo).has(any());
+    }
+
+    @Test
+    void getById_ExistingId_ShouldCallRepositoryMethod() {
+        when(repo.getById(apartmentAvailableId)).thenReturn(Optional.of(apartmentAvailable));
+
+        target.getById(apartmentAvailable.getId());
+
+        verify(repo).getById(apartmentAvailable.getId());
+    }
+
+    @Test
+    void getById_NotExistingId_ShouldThrowsApartmentNotfoundException() {
+        final UUID id = UUID.randomUUID();
+        when(repo.getById(any())).thenReturn(Optional.empty());
+
+        assertThrows(ApartmentNotfoundException.class, () -> target.getById(id));
+        verify(repo).getById(id);
+    }
+
+    @Test
+    void adjust_ExistingIdAndAvailable_ShouldCallRepositoryMethods_GetByIdAndUpdate() {
+        final BigDecimal newPrice = BigDecimal.ONE;
+        ArgumentCaptor<Apartment> captor = ArgumentCaptor.forClass(Apartment.class);
+        when(repo.getById(apartmentAvailableId)).thenReturn(Optional.of(apartmentAvailable));
+
+        target.adjust(apartmentAvailable.getId(), newPrice);
+
+        verify(repo).getById(apartmentAvailable.getId());
+        verify(repo).update(captor.capture());
+        assertThat(captor.getValue().getId()).isEqualTo(apartmentAvailable.getId());
+        assertThat(captor.getValue().getPrice()).isEqualTo(newPrice);
+        assertThat(captor.getValue().getCapacity()).isEqualTo(apartmentAvailable.getCapacity());
+        assertThat(captor.getValue().getStatus()).isEqualTo(apartmentAvailable.getStatus());
+        assertThat(captor.getValue().isAvailability()).isEqualTo(apartmentAvailable.isAvailability());
+    }
+
+    @Test
+    void adjust_ExistingIdAndUnavailable_ShouldCallRepositoryMethod_ThenThrowsApartmentNotfoundException() {
+        final BigDecimal newPrice = BigDecimal.ONE;
+
+        when(repo.getById(apartmentUnavailableId)).thenReturn(Optional.of(apartmentUnavailable));
+
+        assertThrows(ApartmentNotfoundException.class, () -> target.adjust(apartmentUnavailableId, newPrice));
+        verify(repo).getById(apartmentUnavailable.getId());
+    }
+
+    @Test
+    void adjust_NotExistingId_ShouldCallRepositoryMethod_ThenThrowsApartmentNotfoundException(){
+        final BigDecimal newPrice = BigDecimal.ONE;
+
+        when(repo.getById(any())).thenReturn(Optional.empty());
+
+        assertThrows(ApartmentNotfoundException.class, () -> target.adjust(apartmentUnavailableId, newPrice));
+        verify(repo).getById(apartmentUnavailable.getId());
+    }
+
+    @Test
+    void list_ShouldCallRepositoryMethod() {
+        ArgumentCaptor<ApartmentSort> captor = ArgumentCaptor.forClass(ApartmentSort.class);
+        ApartmentSort sort = ApartmentSort.ID;
+
+        target.list(sort);
+
+        verify(repo).findAllSorted(captor.capture());
+        assertThat(captor.getValue()).isEqualTo(sort);
+    }
+
+    @Test
+    void save_WithNonExistingId_ShouldCallRepositoryMethods() {
+        ArgumentCaptor<Apartment> captor = ArgumentCaptor.forClass(Apartment.class);
+        when(repo.has(any())).thenReturn(false);
+
+        target.save(apartmentAvailable);
+
+        verify(repo).has(apartmentAvailable.getId());
+        verify(repo).save(captor.capture());
+        assertThat(captor.getValue()).isEqualTo(apartmentAvailable);
+    }
+
+    @Test
+    void save_WithExistingId_ShouldThrowApartmentWithSameIdExists() {
+        when(repo.has(any())).thenReturn(true);
+
+        assertThrows(ApartmentWithSameIdExists.class, () -> target.save(apartmentAvailable));
+        verify(repo).has(apartmentAvailable.getId());
+    }
+
+    @Test
+    void update_WithExistingId_ShouldCallRepositoryMethods() {
+        ArgumentCaptor<Apartment> captor = ArgumentCaptor.forClass(Apartment.class);
+        when(repo.has(any())).thenReturn(true);
+
+        target.update(apartmentAvailable);
+
+        verify(repo).has(apartmentAvailable.getId());
+        verify(repo).update(captor.capture());
+        assertThat(captor.getValue()).isEqualTo(apartmentAvailable);
+    }
+
+    @Test
+    void save_WithNonExistingId_ShouldThrowApartmentNotfoundException() {
+        when(repo.has(any())).thenReturn(false);
+
+        assertThrows(ApartmentNotfoundException.class, () -> target.update(apartmentAvailable));
+        verify(repo).has(apartmentAvailable.getId());
     }
 }
