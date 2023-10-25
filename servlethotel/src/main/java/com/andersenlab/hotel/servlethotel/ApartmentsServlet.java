480ff26864640538e04d12aa2188ad723a9a4fc5
AnnYaniv <yaniv.anya@gmail.com>
2023-10-25 21:28:31 +0200

    add DELETE method in ApartmentsServlet

--- servlethotel/src/main/java/com/andersenlab/hotel/servlethotel/ApartmentsServlet.java
+++ servlethotel/src/main/java/com/andersenlab/hotel/servlethotel/ApartmentsServlet.java
@@ -6,6 +6,7 @@ import com.andersenlab.hotel.model.ApartmentSort;
 import com.andersenlab.hotel.model.ApartmentStatus;
 import com.andersenlab.hotel.service.CrudService;
 import com.andersenlab.hotel.usecase.ListApartmentsUseCase;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.EnumUtils;
 
 import java.math.BigDecimal;
@@ -32,6 +33,12 @@ public class ApartmentsServlet extends JsonServlet {
         );
         apartmentService.save(apartment);
         return new Response(apartment);
+    }
+
+    @Override
+    Response delete(String uri, Map<String, String> body) {
+        apartmentService.delete(UUID.fromString(body.get("id")));
+        return new Response(HttpServletResponse.SC_OK);
     }
 
     @Override
