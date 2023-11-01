8c34c4620d0093301e0f5ccbfb26aa19420d1fbf
AnnYaniv <yaniv.anya@gmail.com>
2023-11-01 12:35:11 +0100

    Change initContext-method

--- application/src/main/java/com/andersenlab/hotel/Main.java
+++ application/src/main/java/com/andersenlab/hotel/Main.java
@@ -1,23 +1,8 @@
 package com.andersenlab.hotel;
 
-import com.andersenlab.hotel.model.Apartment;
-import com.andersenlab.hotel.model.ApartmentSort;
-import com.andersenlab.hotel.model.Client;
-import com.andersenlab.hotel.model.ClientSort;
 import com.andersenlab.hotel.reader.PropertyReaderFromFile;
-import com.andersenlab.hotel.repository.SortableCrudRepository;
-import com.andersenlab.hotel.repository.infile.InFileApartmentRepository;
-import com.andersenlab.hotel.repository.infile.InFileClientRepository;
-import com.andersenlab.hotel.service.impl.ApartmentService;
-import com.andersenlab.hotel.service.impl.ClientService;
+import com.andersenlab.hotel.service.ContextBuilder;
 import com.andersenlab.hotel.http.ServletStarter;
-import com.andersenlab.hotel.usecase.CheckInClientUseCase;
-import com.andersenlab.hotel.usecase.CheckOutClientUseCase;
-import com.andersenlab.hotel.usecase.impl.BlockedCheckIn;
-import com.andersenlab.hotel.usecase.impl.BlockedCheckOut;
-import lombok.SneakyThrows;
-
-import java.io.File;
 
 public class Main {
 
@@ -35,35 +20,8 @@ public class Main {
         String location = propertyReaderFromFile.readProperty("location");
         String abilityApartmentToChange = propertyReaderFromFile.readProperty("apartment.change.enabled");
 
-        final File file = getFile(location);
-        final SortableCrudRepository<Apartment, ApartmentSort> apartmentRepository = new InFileApartmentRepository(file);
-        final SortableCrudRepository<Client, ClientSort> clientRepository = new InFileClientRepository(file);
-
-        final ApartmentService apartmentService = new ApartmentService(apartmentRepository);
-        final ClientService clientService = new ClientService(clientRepository, apartmentService);
-
-        CheckInClientUseCase checkInClientUseCase;
-        CheckOutClientUseCase checkOutClientUseCase;
-
-        if (Boolean.parseBoolean(abilityApartmentToChange)) {
-            checkInClientUseCase = clientService;
-            checkOutClientUseCase = clientService;
-        } else {
-            checkInClientUseCase = new BlockedCheckIn();
-            checkOutClientUseCase = new BlockedCheckOut();
-        }
-
-        return new HotelModule(clientService, apartmentService, apartmentService,
-                clientService, checkOutClientUseCase, checkInClientUseCase, apartmentService,
-                clientService);
-    }
-
-    @SneakyThrows
-    public static File getFile(String path) {
-        final File file = new File(path);
-        if (!file.exists()) {
-            file.createNewFile();
-        }
-        return file;
+        return new ContextBuilder().initFile(location).initServices()
+                .initCheckInCheckOut(Boolean.parseBoolean(abilityApartmentToChange))
+                .build();
     }
 }
